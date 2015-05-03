a173b5eadc07f115d52af9fd002269722cccd5b0
Hilmar <hilmar@hilmarp.com>
2015-05-03 18:56:30 +0000

    Duration filter og smávægis breytingar

--- js/filters.js
+++ js/filters.js
@@ -39,4 +39,59 @@
 		};
 	});
 
+	// Formatta lengd
+	appFilters.filter('duration', function() {
+		return function(input) {
+			// 365, 0:35
+			if (input.indexOf(':') === 1) {
+				var beforeColon = input.substring(0, input.indexOf(':')),
+					afterColon = input.substring(input.indexOf(':') + 1);
+
+				// Ef 1:15, annars 0:15
+				if (parseInt(beforeColon, 10) > 0) {
+					if (afterColon.charAt(0) === '0') {
+						return beforeColon + ' klst. og ' + afterColon.substring(1) + ' mín.';
+					} else {
+						return beforeColon + ' klst. og ' + afterColon + ' mín.';
+					}
+				} else {
+					if (afterColon.charAt(0) === '0') {
+						return afterColon.substring(1) + ' mín.';
+					} else {
+						return afterColon + ' mín.';
+					}
+				}
+			// RÚV og Skjár 1, 00:35:00
+			} else if (input.indexOf(':') === 2) {
+				var first = input.substring(0, input.indexOf(':')),
+					middle = input.substring(input.indexOf(':') + 1, input.lastIndexOf(':')),
+					last = input.substring(input.lastIndexOf(':') + 1);
+
+				// Ef 02:00:00, annars 00:10:00
+				if (parseInt(first, 10) > 0) {
+					// Ef 02, annars 10
+					if (first.charAt(0) === '0') {
+						if (middle.charAt(0) === '0') {
+							return first.substring(1) + ' klst. og ' + middle.substring(1) + ' mín.';
+						} else {
+							return first.substring(1) + ' klst. og ' + middle + ' mín.';
+						}
+					} else {
+						return first + ' klst. og ' + middle + ' mín.';
+					}
+				} else {
+					if (middle.charAt(0) === '0') {
+						return middle.substring(1) + ' mín.';
+					} else {
+						return middle + ' mín.';
+					}
+				}
+
+				return input;
+			} else {
+				return input;
+			}
+		};
+	});
+
 })();
